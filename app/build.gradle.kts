b892bd1cd943171f105891081e4387f2af91c88b
vysengchhay <sengchhaygg@gmail.com>
2026-08-06 12:12:15 +0700

    fin-001: create project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.androidapp.fintrackandroid"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
